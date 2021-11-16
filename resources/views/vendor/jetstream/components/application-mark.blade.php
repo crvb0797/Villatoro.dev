13e28e93bfe7fcc6f0c73c5c36862cfb1774ece7
Carlos Villatoro <crvb0797@gmail.com>
2021-11-16 11:48:07 -0600

    Creamos el navbar del portafolio
    Los enlaces son reactivos para la creación, la plantilla de jetstream ya esta extendida y los botónes de inicio de sesión y registro estan comentados hasta que hagamos el blog

---
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('./img/logo.svg') }}" alt="Logo de Carlos Villatoro">
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins'],
+            },
+            colors:{
+                'metalic-100': '#D6D6D6',
+                'metalic-200': '#A2A2A2',
+                'metalic-900':'#2A2A2A',
+                'yellow-500': '#F5AF31',
+                skyblue: '#3FAFDD',
+                purple: '#8472AD',
+                pink: '#D843AB',
+    
+            },
+        },
+    },
+    
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
